a5c70517ee07c367a3a30bc29ce2321523f0612b
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-01-24 16:54:04 -0800

    add separate function sections in javascript and a comment suggestion for fixing the code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,47 @@
 function numberCheck(userNumber) {
-  var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40];
-  var output = exceptions(userNumber)
-  return output
+  var numbers = [];
+    for (var index = 0; index < userNumber; index++) {
+      numbers.push(index);
+    }
+    return numbers
 };
 
-function exceptions(userNumber) {
-  var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40];
-  var exceptions = [1, 2, 3]
-  var range = numbers.slice(0, userNumber);
-    for (index = 0; index < range.length; index ++) {
-      exceptions.forEach(function(exception) {
-        if (range === exceptions[3]) {
-          range = "I'm sorry, Dave. I'm afraid I can't do that."
-        } else if (range === exceptions[2]) {
-          range = "Boop!" 
-        } else {
-          range ==="Beep!"
-        }
+// Doing all three exception checks did not work in the same loop or function section. Will try separating them.
 
-      });
-    }; 
-    return range;
-};
+ function exceptionThree(userNumber) {
+  var numbers = [];
+  for (var index = 0; index < userNumber; index++) {
+    numbers.push(index);
+
+   var exceptions = [1, 2, 3]
+   var range = numbers.slice(0, userNumber);
+     for (index = 0; index < range.length; index ++) {
+       range.forEach(function(range) {
+         if (range === exceptions[3]) {
+           range = "I'm sorry, Dave. I'm afraid I can't do that."
+         } else if (range === exceptions[2]) {
+           range = "Boop!" 
+         } else {
+           range ==="Beep!"
+         }
+
+       });
+     }; 
+     return range;
+ };
+
+ function exceptionTwo(userNumber) {
+  var numbers = [];
+  for (var index = 0; index < userNumber; index++) {
+    numbers.push(index);
+ }
+
+
+ function exceptionOne(userNumber) {
+  var numbers = [];
+  for (var index = 0; index < userNumber; index++) {
+    numbers.push(index);
+ }
 
 
 
