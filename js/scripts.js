cacd06b7a916c64eaba36d55bcc0371299d7a4a5
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-01-26 23:27:24 -0800

    fix the function for the first exception that replaces any number that includes one with Beep! This commit will also add two variables in the numberCheck function that turns the number range into a string and passes it through the exceptionOne function, connecting both functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,37 @@
-function numberCheck(userNumber) {
-  var numbers = [];
-    for (var index = 0; index < userNumber; index++) {
-      numbers.push(index);
-    }
-    return numbers
+function exceptionThree(userNumber) {
 };
 
-// Doing all three exception checks did not work in the same loop or function section. Will try separating them.
+function exceptionTwo(userNumber) {
+};
 
- function exceptionThree(userNumber) {
-  var numbers = [];
-  for (var index = 0; index < userNumber; index++) {
-    numbers.push(index);
+function exceptionOne(userNumber) {
+  if (userNumber.includes(1)) {
+    var output = ("Beep!");
+    return output;
+  } else {
+    var output = userNumber;
+    return output;
+  };
+};
 
-   var exceptions = [1, 2, 3]
-   var range = numbers.slice(0, userNumber);
-     for (index = 0; index < range.length; index ++) {
-       range.forEach(function(range) {
-         if (range === exceptions[3]) {
-           range = "I'm sorry, Dave. I'm afraid I can't do that."
-         } else if (range === exceptions[2]) {
-           range = "Boop!" 
-         } else {
-           range ==="Beep!"
-         }
-
-       });
-     }; 
-     return range;
- };
-
- function exceptionTwo(userNumber) {
-  
-
-
- function exceptionOne(userNumber) {
-  
-
-
+function numberCheck(userNumber) {
+  var range = [];
+    for (var index = 0; index < userNumber; index++) {
+      var index = index.toString();
+      var display = exceptionOne(index);
+            range.push(display);
+    }
+    return range
+};
+      
 $(document).ready(function() {
   $("form#input").submit(function(event) {
     event.preventDefault();
     var userNumber = $("input#number").val();
     if (!userNumber) {
-      alert("Please enter a number.");
+            alert("Please enter a number.");
     } else {
-        alert(numberCheck(userNumber));
+            alert(numberCheck(userNumber));
     };
   });
 });
